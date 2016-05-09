f827da1903a49d1bd69e7e444c008f91724c2e17
marc souchet <marcsouchet@MacBook-Pro-de-marc.local>
2016-05-09 13:49:39 +0200

    first commit

---
+++ NikeBoots.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        autoplay: 3000,
+        loop: true,
+        paginationClickable: true,
+        slidesPerView: 4
+        
+    
+    });
